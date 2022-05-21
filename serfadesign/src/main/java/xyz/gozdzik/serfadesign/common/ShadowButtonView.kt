29c3e968294db9625c26bcd4f87d8e8be7111a13
Szymon Gozdzik <szymon@gozdzik.xyz>
2022-05-21 20:44:25 +0200

    Add single bottom choice fragment & shadow button view

---
+++ serfadesign/src/main/java/xyz/gozdzik/serfadesign/common/ShadowButtonView.kt
@@ -0,0 +1,53 @@
+package xyz.gozdzik.serfadesign.common
+
+import xyz.gozdzik.serfadesign.databinding.ViewShadowButtonBinding
+
+import android.content.Context
+import android.content.res.ColorStateList
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import xyz.gozdzik.serfadesign.R
+
+class ShadowButtonView : ConstraintLayout {
+
+    private lateinit var binding: ViewShadowButtonBinding
+
+    constructor(context: Context) : super(context) {
+        init(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init(context, attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init(context, attrs)
+    }
+
+    private fun init(context: Context, attrs: AttributeSet? = null) {
+        binding = ViewShadowButtonBinding.inflate(
+            LayoutInflater.from(context),
+            this,
+            true
+        )
+        val attrs = attrs ?: return
+        context.obtainStyledAttributes(attrs, R.styleable.ShadowButtonView).apply {
+            if (hasValue(R.styleable.ShadowButtonView_buttonTitle)) {
+                binding.buttonTitle.text = getString(R.styleable.ShadowButtonView_buttonTitle)
+            }
+            if (hasValue(R.styleable.ShadowButtonView_buttonColor)) {
+                binding.rootView.backgroundTintList =
+                    ColorStateList.valueOf(
+                        getColor(R.styleable.ShadowButtonView_buttonColor, 0)
+                    )
+            }
+            recycle()
+        }
+    }
+
+}
